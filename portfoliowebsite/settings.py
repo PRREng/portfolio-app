33f0a6f8ccda29f415ff5eacc4c4673e934fc6d5
Pedro Rocha Ribeiro <pr.rocha70@gmail.com>
2024-06-08 13:25:19 -0300

    adding ckeditor

--- portfoliowebsite/settings.py
+++ portfoliowebsite/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'base.apps.BaseConfig',
+
+    'ckeditor',
+    'ckeditor_uploader',
 ]
 
 MIDDLEWARE = [
@@ -126,6 +129,7 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
+CKEDITOR_UPLOAD_PATH = 'uploads/'
 
 
 # Default primary key field type
